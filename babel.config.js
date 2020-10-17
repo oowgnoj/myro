c1d6e2254db3d6cfb30e98cc476a0652fd2e030f
oowgnoj <mangtaob@gmail.com>
2020-10-17 13:46:45 +0900

    add absolute path alias

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,20 @@
 
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugin: [
-    'module-resolver',
-    {
-      root: ['./src'],
-      extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-      alias: {
-        tests: ['./tests/'],
-        '@components': './src/components',
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@containers': './src/containers',
+          '@atoms': './src/components/atoms',
+          '@molecules': './src/components/molecules',
+          '@screens': './src/components/screens',
+        },
       },
-    },
+    ],
   ],
 };
