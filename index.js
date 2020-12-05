c7fb3e960ab841a3446bcdb66550f405bd099ecf
oowgnoj <mangtaob@gmail.com>
2020-12-05 19:02:10 +0900

    add notification

--- index.js
+++ index.js
@@ -2,8 +2,27 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import PushNotification from 'react-native-push-notification';
+
+// NOTIFICAFION SETUP
+PushNotification.configure({
+  onRegister: function (token) {
+    console.log('TOKEN: ', token);
+  },
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  // asking IOS permission
+  requestPermissions: Platform.OS === 'ios',
+});
 
 AppRegistry.registerComponent(appName, () => App);
